b9521a6ea79a4a443767968a2e63a1cc685b208d
Johan Schalkwyk <johan.schalkwyk@gmail.com>
2017-05-30 20:37:48 -0400

    Basic flash card JS / Electron example

---
+++ flashcard.js
@@ -0,0 +1,66 @@
+var console = require('console');
+
+class FlashCard {
+    constructor(hint, answer) {
+        this.hint = hint;
+        this.answer = answer;
+    }
+};
+
+class FlashCardGame {
+    constructor() {
+        this.cards = [];
+        this.score = {correct: 0, wrong: 0 };
+        this.cardText = document.getElementById("cardHint");
+        this.submitButton = document.getElementById("cardSubmit");
+        this.rightText =  document.getElementById("scoreRight");
+        this.wrongText = document.getElementById("scoreWrong");
+        this.index = 0;
+    }
+
+    addCard(card) { this.cards.push(card); }
+    getCard(index) { return this.cards[index]; }
+
+    cardMove(count) {
+        this.index += count;
+        if (this.index >= this.cards.length) {
+            this.index = 0;
+        }
+        if (this.index < 0) {
+            this.index = 0;
+        }
+        this.cardUpdate();
+    }
+
+    cardUpdate() {
+        var curCard = this.cards[this.index];
+        console.log(curCard.hint);
+        this.cardText.innerHTML = curCard.hint;
+        this.rightText.innerHTML = this.score.correct.toString();
+        this.wrongText.innerHTML = this.score.wrong.toString();
+    }
+
+    checkAnswer() {
+        var guess = document.getElementById("cardAnswer").value;
+        var answer = this.cards[this.index].answer;
+        if (guess === answer) {
+            this.score.correct++;
+            console.log("you got it!!");
+        } else {
+            this.score.wrong++;
+            console.log("nahh!");
+        }
+        console.log(this.score);
+    }
+};
+
+var game = new FlashCardGame();
+game.addCard(new FlashCard("dog", "bark"));
+game.addCard(new FlashCard("cat", "meouw"));
+
+console.log(game.getCard(1));
+game.cardUpdate();
+
+game.submitButton.addEventListener('click', function() {
+    game.checkAnswer();
+    game.cardMove(1);} );
